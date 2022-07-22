1b90c2a4f455c9595f63e2e02fe49fdf28994d19
Hauwa <79663650+Hauwa303@users.noreply.github.com>
2022-07-23 00:47:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let saveEL = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment() {
+    count  += 1; 
+    countEl.textContent = count;
+
+    
+}
+function save(){
+     let memory = count  + " - ";
+     //textContent returns every element in the node 
+     saveEL.textContent += memory;
+     countEl.textContent = 0
+     count = 0
+}
+ 
+
+
+
+
+
+
+
+
+
